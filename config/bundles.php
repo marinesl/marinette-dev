1907622bcdc75680d2b3291f7b4dbc7c16a949b9
Marine Lancelin <lancelinmarine@gmail.com>
2023-03-29 15:33:30 +0200

    :heavy_plus_sign: Add ORM fixture dependency

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
