8b5bdcada83ff0b9ca7141a4b473fc746d57adb6
Marine Lancelin <lancelinmarine@gmail.com>
2023-03-29 15:45:31 +0200

    :card_file_box: Create Status DataFixture

---
+++ src/DataFixtures/Status.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Status as EntityStatus;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class Status extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $status = new EntityStatus();
+        $status->setName('Publié');
+        $manager->persist($status);
+
+        $status = new EntityStatus();
+        $status->setName('Brouillon');
+        $manager->persist($status);
+
+        $status = new EntityStatus();
+        $status->setName('En relecture');
+        $manager->persist($status);
+
+        $status = new EntityStatus();
+        $status->setName('Corbeille');
+        $manager->persist($status);
+
+        $status = new EntityStatus();
+        $status->setName('Supprimé');
+        $manager->persist($status);
+
+        $manager->flush();
+    }
+}
